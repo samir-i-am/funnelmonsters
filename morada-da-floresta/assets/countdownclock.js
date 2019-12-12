8834141ff56274ec22a53cc0c595c20f232d8eca
avelar <fortunato.avelar@gmail.com>
2019-12-12 09:17:22 -0300

    update countdown; added new image; change landing page content

--- morada-da-floresta/assets/countdownclock.js
+++ morada-da-floresta/assets/countdownclock.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 25, 2019 24:00:00").getTime();
+var countDownDate = new Date("Dec 24, 2019 24:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
